03fea59e77e4f50fae07cdef403ab97874143612
Felicio Hoffmann <felicio.pereira@lsitec.org.br>
2022-03-18 17:33:43 -0300

    Fixing bugs with the edition and creation of new entries

--- app/src/main/java/com/example/level1/NewStudentActivity.java
+++ app/src/main/java/com/example/level1/NewStudentActivity.java
@@ -2,6 +2,7 @@ package com.example.level1;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,12 +12,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.level1.dao.StudentDAO;
 import com.example.level1.model.Student;
 
-public class NewStudentActivity extends AppCompatActivity{
+public class NewStudentActivity extends AppCompatActivity {
 
     private EditText nameField;
     private EditText phoneField;
     private EditText emailField;
     private Student studentedit;
+    private Intent data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,24 +27,40 @@ public class NewStudentActivity extends AppCompatActivity{
         setTitle("Adicione novo aluno");
         setContentView(R.layout.activity_new_student);
         getFields();
+        configureSaveButton(dao);
 
-        Intent data = getIntent();
-        studentedit = (Student) data.getSerializableExtra("student");
-//        dao.edit(studentedit);
+        data = getIntent();
+        Log.i("teste", "onCreate: " + data.getExtras());
+        if (data.getExtras() != null) {
+            studentedit = (Student) data.getSerializableExtra("student");
+            nameField.setText(studentedit.getName());
+            phoneField.setText(studentedit.getPhone());
+            emailField.setText(studentedit.getEmail());
+        }
 
+    }
 
+    private void configureSaveButton(StudentDAO dao) {
         Button saveButton = findViewById(R.id.activity_new_student_save);
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                createAndSaveStudent();
-                dao.edit(studentedit);
-                finish();
+                Log.i("teste", "onClick: " + data.getData());
+                if (data.getExtras() != null) {
+                    editStudent();
+                    dao.edit(studentedit);
+                    finish();
+                }
+                else{
+                    Student student = new Student(nameField.getText().toString(), phoneField.getText().toString(), emailField.getText().toString());
+                    dao.save(student);
+                    finish();
+                }
             }
         });
     }
 
-    private void createAndSaveStudent() {
+    private void editStudent() {
         String name = nameField.getText().toString();
         String phone = phoneField.getText().toString();
         String email = emailField.getText().toString();
