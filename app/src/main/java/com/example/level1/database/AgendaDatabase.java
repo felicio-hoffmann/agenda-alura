9eac6f5aa3642d64804a5d3f0ce813e28596d471
Felicio Hoffmann <felicio.pereira@lsitec.org.br>
2022-04-07 12:54:32 -0300

    Realizando a mudança do DB utilizando Migration

--- app/src/main/java/com/example/level1/database/AgendaDatabase.java
+++ app/src/main/java/com/example/level1/database/AgendaDatabase.java
@@ -2,9 +2,12 @@ package com.example.level1.database;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.example.level1.model.Student;
 
@@ -20,7 +23,12 @@ public abstract class AgendaDatabase extends RoomDatabase {
         if (instance == null) {
             instance = Room.databaseBuilder(context, AgendaDatabase.class, DB_NAME)
                     .allowMainThreadQueries()
-                    .fallbackToDestructiveMigration()
+                    .addMigrations(new Migration(1, 2) {
+                        @Override
+                        public void migrate(@NonNull SupportSQLiteDatabase database) {
+                            database.execSQL("ALTER TABLE student ADD COLUMN surname TEXT");
+                        }
+                    })
                     .build();
         }
         return instance;
