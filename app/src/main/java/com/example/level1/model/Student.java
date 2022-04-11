a0a755911c9624b25ee3b4ba213e1c810a0604e1
Felicio Hoffmann <felicio.pereira@lsitec.org.br>
2022-04-11 18:06:26 -0300

    [#1][Mensagem commit] Teste de Amend com AndroidStudio
    Pulando linha para detalhar as alterações feitas no commit e quando tem uma mensagem muito grande
    
    Adicionando tambem a assinatura
    
    Signed-off-by: Felicio Hoffmann <felicio.pereira@lsitec.org.br>

--- app/src/main/java/com/example/level1/model/Student.java
+++ app/src/main/java/com/example/level1/model/Student.java
@@ -13,6 +13,7 @@ public class Student implements Serializable {
     private  String name;
     private String surname;
     private  String email;
+    //teste com assinatura com ammend
 
     public Student(String name, String phone, String email) {
         this.name = name;
